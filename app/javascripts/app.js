152fc5ad28a4df86a6f2d8e5bcf1dc1273c8f5f7
drewstone <drewstone329@gmail.com>
2016-03-17 23:37:32 -0400

    Update app.js

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,58 +1,6 @@
 var accounts;
 var account;
-var balance;
 
-function setStatus(message) {
-  var status = document.getElementById("status");
-  status.innerHTML = message;
-};
-
-function refreshBalance() {
-  var meta = MetaCoin.deployed();
-
-  meta.getBalance.call(account, {from: account}).then(function(value) {
-    var balance_element = document.getElementById("balance");
-    balance_element.innerHTML = value.valueOf();
-  }).catch(function(e) {
-    console.log(e);
-    setStatus("Error getting balance; see log.");
-  });
-};
-
-function getBalance(address) {
-  var meta = MetaCoin.deployed();
-
-  meta.getBalance.call(address, {from: address}).then(function(value) {
-    var elt = "<li>" + address + " | " +  value.valueOf() +  "</li>";
-    $('#allAccounts').append(elt);
-  }).catch(function(e) {
-    console.log(e);
-      var elt = "<li>" + address + " | " +  0 +  "</li>";
-  });
-}
-
-function sendCoin() {
-  var meta = MetaCoin.deployed();
-
-  var amount = parseInt(document.getElementById("amount").value);
-  var receiver = document.getElementById("receiver").value;
-
-  setStatus("Initiating transaction... (please wait)");
-
-  meta.sendCoin(receiver, amount, {from: account}).then(function() {
-    setStatus("Transaction complete!");
-    refreshBalance();
-  }).catch(function(e) {
-    console.log(e);
-    setStatus("Error sending coin; see log.");
-  });
-};
-
-function refreshAll() {
-  for (var i = 0; i < accounts.length; i++) {
-      getBalance(accounts[i]);
-  }
-}
 
 window.onload = function() {
   web3.eth.getAccounts(function(err, accs) {
@@ -68,8 +16,5 @@ window.onload = function() {
 
     accounts = accs;
     account = accounts[0];
-
-    refreshBalance();
-    refreshAll();
   });
 }
